5decc4c42f95b5a9d9746eba029a9263f01046bb
Muhammad Miftah <mamift@gmail.com>
2023-03-06 11:19:54 +1000

    Added GetLinqSearchConfig() method.

--- src/Linq.Search/Extensions/ConfigurationExtensions.cs
+++ src/Linq.Search/Extensions/ConfigurationExtensions.cs
@@ -6,6 +6,26 @@ namespace CityofEdmonton.Linq.Search.Extensions
     public static class ConfigurationExtensions
     {
         /// <summary>
+        /// This convenience method will throw an exception an instance of <see cref="SearchConfigurationOptions"/> was not found in the current <see cref="IServiceProvider"/>.
+        /// <para>Use <see cref="IServiceProvider.GetService"/> to handle null values without throwing an exception.</para>
+        /// </summary>
+        /// <param name="sp"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static SearchConfigurationOptions GetLinqSearchConfig(this IServiceProvider sp)
+        {
+            var searchConfigurationOptions = sp.GetService<SearchConfigurationOptions>();
+            if (searchConfigurationOptions == null)
+            {
+                throw new InvalidOperationException(
+                    $"No instance of {nameof(SearchConfigurationOptions)} was found in the Service Provider. " +
+                    $"Have you run any of the configuration extension methods? (like {nameof(ConfigureLinqSearch)}?)");
+            }
+
+            return searchConfigurationOptions;
+        }
+
+        /// <summary>
         /// Configure the Linq.Search library using <see cref="SearchConfigurationOptions"/>, but also allow access to the underlying <see cref="IServiceProvider"/>.
         /// <para>Defaults to configuring the <see cref="SearchConfigurationOptions"/> as a <see cref="ServiceLifetime.Singleton"/>.</para>
         /// </summary>
